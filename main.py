e8a242fc2b6cc1ddcb7a787d63c409330877b0ed
Aspetere <pedrinhoalgusto07@gmail.com>
2022-11-08 00:22:42 -0300

    script placeholder para pygame

---
+++ main.py
@@ -0,0 +1,9 @@
+from settings import *
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    pygame.display.update()
---
+++ settings.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.init()
+
+width, height = 800, 800
+screen = pygame.display.set_mode((width, height))
+FPS = 60
+clock = pygame.time.Clock()
