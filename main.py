9d0a3b59467cb394dcac89a3d5a5b7f0b0792381
Aspetere <pedrinhoalgusto07@gmail.com>
2022-11-15 13:19:03 -0300

    added font some fixes

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from settings import *
+from functions import *
 
 while True:
+    screen.fill("black")
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     
+
     pygame.display.update()
--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ width, height = 800, 800
 screen = pygame.display.set_mode((width, height))
 FPS = 60
 clock = pygame.time.Clock()
+font_size = 15
+font = pygame.font.Font(pygame.font.get_default_font(), font_size)
